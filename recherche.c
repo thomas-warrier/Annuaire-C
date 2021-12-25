38cc083516bb9abc24b48c80fcc088236e0df577
thomas-warrier <81181413+thomas-warrier@users.noreply.github.com>
2021-12-25 21:54:27 +0100

    la fonction de filtre marche correctement j'ai finalement fais d'une autre facon qu'en modifiant le code source de la fonction strstr() mais cela fonctionne execatement de la même maniére je suis content. Ca marche !

--- recherche.c
+++ recherche.c
@@ -8,8 +8,8 @@ int chercher_par()
     return choix_information;
 }
 
-char* par_quoi_chercher(client *clients,int choix_information)
-{  
+char *par_quoi_chercher(client *clients, int choix_information)
+{
     switch (choix_information)
     {
     case 1:
@@ -23,42 +23,74 @@ char* par_quoi_chercher(client *clients,int choix_information)
     }
 }
 
-int strcmp_minuscule (const char *p1, const char *p2)   //j'ai repris le code source de la fonction strcmp et je l'ai modifié en ajoutant en faisant en sorte qu'au moment de la comparaison il compare les caractére en minuscule.
+int strcmp_minuscule(const char *p1, const char *p2) //j'ai repris le code source de la fonction strcmp et je l'ai modifié en ajoutant en faisant en sorte qu'au moment de la comparaison il compare les caractére en minuscule.
 {
-  const unsigned char *s1 = (const unsigned char *) p1;
-  const unsigned char *s2 = (const unsigned char *) p2;
-  unsigned char c1, c2;
-  do
+    const unsigned char *s1 = (const unsigned char *)p1;
+    const unsigned char *s2 = (const unsigned char *)p2;
+    unsigned char c1, c2;
+    do
     {
-      c1 = tolower((unsigned char) *s1++);
-      c2 = tolower((unsigned char) *s2++);
-      if (c1 == '\0')
-        return c1 - c2;
-    }
-  while (c1 == c2);
-  return c1 - c2;
+        c1 = tolower((unsigned char)*s1++);
+        c2 = tolower((unsigned char)*s2++);
+        if (c1 == '\0')
+            return c1 - c2;
+    } while (c1 == c2);
+    return c1 - c2;
 }
 
-void recherche_sequentielle(client clients[], char* recherche,int choix_information,int nombre_client)
+void recherche_sequentielle(client clients[], char *recherche, int choix_information, int nombre_client)
 {
-    int i=0;
-    while (i< nombre_client){
-        if (strcmp_minuscule((par_quoi_chercher(&clients[i],choix_information)),recherche) == 0 ){
-            printf("\n%s,%s,%s,%s,%s,%s,%s",clients[i].prenom,clients[i].nom,clients[i].ville,clients[i].code_postal,clients[i].num_de_tel,clients[i].adresse_mail,clients[i].profession);
+    int i = 0;
+    while (i < nombre_client)
+    {
+        if (strcmp_minuscule((par_quoi_chercher(&clients[i], choix_information)), recherche) == 0)
+        {
+            printf("\n%s,%s,%s,%s,%s,%s,%s", clients[i].prenom, clients[i].nom, clients[i].ville, clients[i].code_postal, clients[i].num_de_tel, clients[i].adresse_mail, clients[i].profession);
         }
-        i++;        
+        i++;
     }
 }
 
-void filtre(client clients[],char* mot_filtre,int choix_information,int nombre_client)
+void filtre(client clients[], char *mot_filtre, int choix_information, int nombre_client)
 {
-    int i=0;
-    char* p;
-    while (i< nombre_client){
-        p=strstr((par_quoi_chercher(&clients[i],choix_information)),mot_filtre);
-        if (p){
-            printf("\n%s",p);
-        }
-        i++;        
+    /*
+    nous voulons que notre fonction ne prenne pas en compte les majuscules,minuscule.
+    par exemple si je rentre "ber" en filtre et qu'il existe Bertrand je dois le trouver.
+    seulement le probléme est que la fonction strstr() fait une comparaison dans l'état actuelle
+    de la chaine de caractére en étant sensible aux majuscules et minuscules.
+    mon idée de base était de trouver le code source de la fonction strstr() afin de le modifier et qu'il effectue la 
+    comparaison en lowercase,j'ai trouver certains code source sur internet mais je n'ais pas vraiment réussis a les comprendres car ils faisaient appelent
+    a des connaissance que je n'ai pas. j'ai donc trouver l'alternative suivante
+    */
+    int i = 0;
+    char *p;
+
+    size_t len = strlen(mot_filtre);
+    char *mot_filtre_minuscule = calloc(len + 1, sizeof(char));
+    for (size_t i = 0; i < len; ++i)
+    {
+        mot_filtre_minuscule[i] = tolower((unsigned char)mot_filtre[i]);
     }
+
+    while (i < nombre_client)
+    {
+        char *info = par_quoi_chercher(&clients[i], choix_information);
+
+        size_t len = strlen(info);
+        char *info_minuscule = calloc(len + 1, sizeof(char));
+
+        for (size_t i = 0; i < len; ++i)
+        {
+            info_minuscule[i] = tolower((unsigned char)info[i]);
+        }
+
+        p = strstr(info_minuscule, mot_filtre_minuscule);
+        if (p)
+        {
+            printf("a la ligne %d : %s \n",i,par_quoi_chercher(&clients[i], choix_information));
+        }
+        i++;
+        free(info_minuscule);
+    }
+    free(mot_filtre_minuscule);
 }
